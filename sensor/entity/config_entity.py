173ad562c0c3c0a53801bbec660220c6e334f00d
Abhishek Kulkarni <kul.abhishake@gmail.com>
2023-01-02 18:29:40 +0530

    Data Validation Completed

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -12,6 +12,7 @@ class TrainingPipelineConfig:
         self.timestamp: str = timestamp
 
 
+
 class DataIngestionConfig:
 
     def __init__(self, training_pipeline_config = TrainingPipelineConfig):
@@ -35,3 +36,23 @@ class DataIngestionConfig:
         self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
         
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
+
+
+
+
+
+class DataValidationConfig:
+
+    def __init__(self, training_pipeline_config = TrainingPipelineConfig):
+        self.data_validation_dir:str = os.path.join(training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
+        self.valid_data_dir:str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir:str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_INVALID_DIR)
+        self.vaild_train_file_path = os.path.join(self.valid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.vaild_train_file_path = os.path.join(self.valid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.invaild_train_file_path = os.path.join(self.invalid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.invaild_train_file_path = os.path.join(self.invalid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.drift_report_file_path:str = os.path.join(
+            self.data_validation_dir,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME
+        )
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,9 +1,10 @@
-from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
 from sensor.exception import SensorException
-from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 import sys,os
 from sensor.logger import logging
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
 
 class TrainPipeline:
@@ -23,9 +24,15 @@ class TrainPipeline:
         except Exception as e:
             raise SensorException(e,sys)
 
-    def start_data_validation(self):
+    def start_data_validation(self, data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
         try:
-            pass
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+            data_validation_config = data_validation_config
+            )
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info(f"Data validation completed and artifact created: {data_validation_artifact}")
+            return data_validation_artifact
         except Exception as e:
             raise SensorException(e,sys)
 
@@ -57,6 +64,7 @@ class TrainPipeline:
     def run_pipeline(self):
         try:
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
 
         except Exception as e:
             raise SensorException(e,sys)
